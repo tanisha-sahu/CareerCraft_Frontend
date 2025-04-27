579aeb14264a92c72663c86a15622ec54cdacf2c
tanisha-sahu <tanishasahu205@gmail.com>
2025-04-27 13:13:58 +0530

    Changes

--- src/components/landing/PublicLayout.tsx
+++ src/components/landing/PublicLayout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import InfiniteScrollBar from "./InfiniteScrollBar";
 
 const PublicLayout = () => {
   return (
     <>
-      <InfiniteScrollBar />
       <Navbar />
       <Outlet />
       <Footer />
--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/landing/Navbar'; // adjust path as needed
 import Footer from '../components/landing/Footer'; // adjust path as needed
-// import InfiniteScrollBar from '../components/landing/InfiniteScrollBar';
+
 
 const MainLayout: React.FC = () => {
   return (
